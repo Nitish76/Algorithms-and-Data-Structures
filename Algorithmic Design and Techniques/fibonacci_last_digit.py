17ba9fb1cbf2be8714c343534047c17490dbcd11
Nitish Goyal <36237103+Nitish76@users.noreply.github.com>
2018-09-24 20:03:05 -0500

    Problem description
    Task, program description, constraints and comments added

--- Algorithmic Design and Techniques/fibonacci_last_digit.py
+++ Algorithmic Design and Techniques/fibonacci_last_digit.py
@@ -1,4 +1,11 @@
 # Uses python3
+
+''' Task: Given an integer 𝑛, find the last digit of the 𝑛th Fibonacci number'''
+''' A simple and efficient program that computes the last digit of the nth fibonacci number.
+    Note that the code does not use recusrion which would waste
+    memory due to multiple function calls'''
+''' Constraint - 0 <= n <= 10.0e+7'''
+
 import sys
 
 def get_fibonacci_last_digit_naive(n):
@@ -7,12 +14,14 @@ def get_fibonacci_last_digit_naive(n):
 
     previous = 0
     current  = 1
-
-    for _ in range(n - 1):
+    
+    # the following loop adds only the units digit of 2 consecutive fibonacci numbers
+    for _ in range(n - 1):                                  
         previous, current = current%10, (previous + current)%10
 
     return current % 10
 
+# input output statements
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
