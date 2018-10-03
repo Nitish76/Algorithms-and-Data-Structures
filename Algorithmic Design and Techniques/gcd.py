452da41127dd14dd0f2961af968e11323cb3168c
Nitish Goyal <36237103+Nitish76@users.noreply.github.com>
2018-10-02 20:29:52 -0500

    problem description
    Added task, program description and constraints.

--- Algorithmic Design and Techniques/gcd.py
+++ Algorithmic Design and Techniques/gcd.py
@@ -1,9 +1,13 @@
 # Uses python3
 import sys
+''' Task: Given two integers a and b (both are not 0), find their greatest common divisor.'''
+''' A program to compute the gcd using the Euclidean algorithm of two given integers using recursion'''
+
+'''constraints: 1 <= a,b <= 2*10^9'''
 
 def gcd_naive(a, b):
     if b == 0:
-      return a
+      return a                          # 0 is divisible by any integera
     r= a%b;
     return gcd_naive(b,r)
     
