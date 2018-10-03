f266c43f2a14c78cd0151ac4eb03cbe7eb3dd64d
Nitish Goyal <36237103+Nitish76@users.noreply.github.com>
2018-10-02 20:33:35 -0500

    Problem description
    Added task, program description, comments and constraints.

--- Algorithmic Design and Techniques/lcm.py
+++ Algorithmic Design and Techniques/lcm.py
@@ -1,14 +1,20 @@
 # Uses python3
 import sys
+''' Task: Given two integers a and b, find their least common multiple.'''
+
+'''The least common multiple of two positive integers a and b is the least positive
+integer m that is divisible by both a and b. This program computes the lcm using recursion'''
+
+'''Constraints: 1 <= a,b <= 2*10^9'''
 
 def gcd_naive(a, b):
     if b == 0:
-      return a
+      return a                              # 0 is divisble by any integer
     r= a%b;
-    return gcd_naive(b,r)
+    return gcd_naive(b,r)                   # First the gcd is found
 
 def lcm_naive(a,b):
-    lcm = int(a*b//gcd_naive(a,b));
+    lcm = int(a*b//gcd_naive(a,b));         # lcm = product of integers/gcd
     return lcm
 
 if __name__ == '__main__':
