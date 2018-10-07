a100f0ea01d39528830c9651071903bec00a1da7
Nitish Goyal <36237103+Nitish76@users.noreply.github.com>
2018-10-07 13:22:51 -0500

    Description
    Added problem description, task, constraints, input format and comments

--- Algorithmic Design and Techniques/fractional_knapsack.py
+++ Algorithmic Design and Techniques/fractional_knapsack.py
@@ -1,26 +1,32 @@
 # Uses python3
 import sys
+''' A thief finds much more loot than his bag can fit. Help him to find the most valuable combination
+of items assuming that any fraction of a loot item can be put into his bag.'''
+
+''' Task: The goal of this code problem is to implement an algorithm for the fractional knapsack problem'''
+''' Input format: The first line of the input contains the number n of items and the capacity W of a knapsack.
+The next n lines define the values and weights of the items. The i-th line contains integers vi and wi—the
+value and the weight of i-th item, respectively.'''
+
+''' Constraints: 1 <=n <= 10^3, 0<= W <= 2*10^6, 0<= vi <= 2*10^6, 0<= wi <= 2*10^6
+                all numbers are integers'''
 
 def get_optimal_value(capacity, weights, values):
     value = 0.
     valperwt = [0]*n;
-    for i in range(n):
+    for i in range(n):                          # for loop to store value per weight of every item
       valperwt[i] = values[i]/weights[i];
-    #valperwt = sorted(valperwt, reverse = True);
+    
     while capacity>0 and weights:
-      #if capacity == 0:
-        #return value
       i = valperwt.index(max(valperwt));
       if weights[i]<=capacity:
         capacity -= weights[i];
-        value += values[i];
+        value += values[i];                     # all of the remaining most valuable item can be fit
       else:
         value += (capacity*valperwt[i]);
-        capacity =0;
-      del valperwt[i]; del weights[i]; del values[i];
-    value = round(value,4); print(value);
-   # write your code here
-
+        capacity =0;                            # a portion of the remaining most valuable item can be fit
+      del valperwt[i]; del weights[i]; del values[i];       #the item that was put in the knapsack is removed from the loot
+    value = round(value,4); print(value);                   # max total value of fractions of items in knapsack 
     return value
 
 
