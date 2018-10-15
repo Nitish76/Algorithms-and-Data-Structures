01c6a79498a293f925caad0b51b34ebaa952b45b
Nitish Goyal <36237103+Nitish76@users.noreply.github.com>
2018-10-14 23:15:41 -0500

    Problem description
    Added problem description, task, constraints and formats

--- Algorithmic Design and Techniques/binary_search.py
+++ Algorithmic Design and Techniques/binary_search.py
@@ -1,10 +1,20 @@
 # Uses python3
 import sys
+''' This code implements iterative binary search using the divide and conquer algorithmic technique'''
+
+'''Task: The goal in this code problem is to implement the binary search algorithm.'''
+
+''' Input Format: The first line of the input contains an integer n and a sequence a0 < a1 < . . . < an−1
+of n pairwise distinct positive integers in increasing order. The next line contains an integer k and k
+positive integers b0, b1, . . . , bk−1.'''
+
+'''Constraints: 1 <= n,k <= 10^4, 1 <= ai, bi <= 10^9'''
+'''Output format: For all i from 0 to k − 1, output an index 0 <= j <= n − 1 such that aj = bi or −1 if there
+is no such index.'''
 
 def binary_search(a, x):
     left, right = 0, len(a)-1;
-    #a = True;
-    while left<=right:
+    while left<=right:                         
         mid = left + (right - left)//2;
         if x==a[mid]:
             return mid
@@ -14,13 +24,7 @@ def binary_search(a, x):
             left = mid +1;
     else:
         return -1
-   # write your code here
 
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
-    return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -29,5 +33,4 @@ if __name__ == '__main__':
     m = data[n + 1]
     a = data[1 : n + 1]
     for x in data[n + 2:]:
-        # replace with the call to binary_search when implemented
         print(binary_search(a, x), end = ' ')
