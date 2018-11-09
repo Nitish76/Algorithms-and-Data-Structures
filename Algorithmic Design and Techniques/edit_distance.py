68fa3fd55353dc6ef752f73a3251ed7086a7c8a4
Nitish Goyal <36237103+Nitish76@users.noreply.github.com>
2018-11-08 19:00:32 -0600

    Problem description
    Added problem description, constraints, input and output formats.

--- Algorithmic Design and Techniques/edit_distance.py
+++ Algorithmic Design and Techniques/edit_distance.py
@@ -1,4 +1,16 @@
 # Uses python3
+'''The edit distance between two strings is the minimum number of operations (insertions, deletions, and
+substitutions of symbols) to transform one string into another. It is a measure of similarity of two strings.
+Edit distance has applications, for example, in computational biology, natural language processing, and spell
+checking. The goal in this problem is to compute the edit distance between two strings.'''
+
+'''Input Format: Each of the two lines of the input contains a string consisting of lower case latin letters.'''
+'''Output Format: The edit distance between the given two strings.'''
+
+'''Constraints: The length of both strings is at least 1 and at most 100'''
+
+
+
 def edit_distance(s, t):
     n = len(s); m = len(t);
     D = [[0]*(m+1) for i in range(n+1)]
